6051ac634947ca386b57a831cd84766de548d0b5
kim-se-hee <saree98@nvaer.com>
2025-07-05 22:08:54 +0900

    init: 최초 커밋

---
+++ src/test/java/ksh/emall/EMallApplicationTests.java
@@ -0,0 +1,13 @@
+package ksh.emall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
