8aeaddc2d215386874a4dce6706248053616d1a2
kim-se-hee <saree98@nvaer.com>
2025-07-06 19:13:46 +0900

    feat: 카테고리에 속한 제품 페이지 조회 구현

---
+++ src/main/java/ksh/emall/product/service/ProductService.java
@@ -0,0 +1,28 @@
+package ksh.emall.product.service;
+
+import ksh.emall.product.dto.request.ProductRequestDto;
+import ksh.emall.product.repository.ProductRepository;
+import ksh.emall.product.repository.projection.ProductWithReviewStat;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public Page<ProductWithReviewStat> findProductsByCategory(
+        Pageable pageable,
+        ProductRequestDto productRequest
+    ) {
+        return productRepository.findByCategoryWithReviewStat(
+            pageable,
+            productRequest.getCategory(),
+            productRequest.getCriteria(),
+            productRequest.isAscending()
+        );
+    }
+}
