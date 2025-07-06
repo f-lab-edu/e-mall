df6eacfec0df2b7e8373fb7c2ee638a5e0efdda7
kim-se-hee <saree98@nvaer.com>
2025-07-06 19:15:39 +0900

    feat: 카테고리에 속한 제품 조회 API 구현

---
+++ src/main/java/ksh/emall/product/dto/response/ProductResponseDto.java
@@ -0,0 +1,38 @@
+package ksh.emall.product.dto.response;
+
+import ksh.emall.product.entity.DeliveryType;
+import ksh.emall.product.entity.Product;
+import ksh.emall.product.entity.ProductReviewStat;
+import ksh.emall.product.repository.projection.ProductWithReviewStat;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+public class ProductResponseDto {
+
+    private String name;
+    private int price;
+    private DeliveryType deliveryType;
+    private LocalDate guaranteedDeliveryDate;
+    private double averageScore;
+    private int reviewCount;
+    private String imageUrl;
+
+    public static ProductResponseDto from(ProductWithReviewStat productWithReviewStat) {
+        Product product = productWithReviewStat.getProduct();
+        ProductReviewStat reviewStat = productWithReviewStat.getReviewStat();
+
+        return ProductResponseDto.builder()
+            .name(product.getName())
+            .price(product.getPrice())
+            .deliveryType(product.getDeliveryType())
+            .guaranteedDeliveryDate(LocalDate.now().plusDays(product.getExpectedDeliveryDays()))
+            .averageScore(reviewStat.getAverageReviewScore())
+            .reviewCount(reviewStat.getReviewCount())
+            .imageUrl(product.getImageUrl())
+            .build();
+    }
+}
