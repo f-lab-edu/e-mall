4ecacf383f31d3d9c5ec72b8057bb9f32c8242e5
kim-se-hee <saree98@nvaer.com>
2025-07-06 16:37:20 +0900

    feat: 제품 엔티티 생성

---
+++ src/main/java/ksh/emall/product/entity/DeliveryType.java
@@ -0,0 +1,7 @@
+package ksh.emall.product.entity;
+
+public enum DeliveryType {
+    NORMAL,
+    FAST,
+    FRESH
+}
---
+++ src/main/java/ksh/emall/product/entity/Product.java
@@ -0,0 +1,41 @@
+package ksh.emall.product.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import ksh.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer price;
+
+    private DeliveryType deliveryType;
+
+    private Integer quantity;
+
+    private Integer expectedDeliveryDays;
+
+    private String imageUrl;
+
+    @SQLDelete(sql = "update order set is_deleted = true where id = ?")
+    @Where(clause = "isDeleted = 0")
+    private boolean isDeleted;
+}
