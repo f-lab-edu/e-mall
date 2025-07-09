f3cfcc14398a1dd3febebda9c2156b96feab45b2
kim-se-hee <saree98@nvaer.com>
2025-07-10 00:58:57 +0900

    feat: 예외 메세지 관리를 위한 메세징 기능 도입

---
+++ src/main/java/ksh/emall/common/config/MessageSourceConfig.java
@@ -0,0 +1,24 @@
+package ksh.emall.common.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+@Configuration
+public class MessageSourceConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setFallbackToSystemLocale(false);
+        messageSource.setDefaultLocale(Locale.KOREA);
+        return messageSource;
+    }
+}
