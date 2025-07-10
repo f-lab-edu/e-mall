1426779504ef3d8bc529ba981b0202dc01138108
kim-se-hee <saree98@nvaer.com>
2025-07-11 02:01:28 +0900

    fix: 잘못된 메서드 사용, getByProduct를 getByProductId로 교체

--- src/main/java/ksh/emall/product/repository/ProductReviewStatRepository.java
+++ src/main/java/ksh/emall/product/repository/ProductReviewStatRepository.java
@@ -1,15 +1,18 @@
 package ksh.emall.product.repository;
 
+import jakarta.persistence.LockModeType;
 import ksh.emall.common.exception.CustomException;
 import ksh.emall.common.exception.ErrorCode;
 import ksh.emall.product.entity.ProductReviewStat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductReviewStatRepository extends JpaRepository<ProductReviewStat, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     default ProductReviewStat getByProductId(Long productId) {
         return findByProductId(productId)
             .orElseThrow(() -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND, List.of(productId)));
