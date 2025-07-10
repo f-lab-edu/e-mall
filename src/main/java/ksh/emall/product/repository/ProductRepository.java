59c0d5f2018059a071c67c9d57edb0dd7ee5c1c6
kim-se-hee <saree98@nvaer.com>
2025-07-10 16:07:59 +0900

    feat: id로 제품 조회 기능 추가

--- src/main/java/ksh/emall/product/repository/ProductRepository.java
+++ src/main/java/ksh/emall/product/repository/ProductRepository.java
@@ -1,7 +1,16 @@
 package ksh.emall.product.repository;
 
+import ksh.emall.common.exception.CustomException;
+import ksh.emall.common.exception.ErrorCode;
 import ksh.emall.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductQueryRepository {
+
+    default Product getById(Long id) {
+        return findById(id)
+            .orElseThrow(() -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND, List.of(id)));
+    }
 }
