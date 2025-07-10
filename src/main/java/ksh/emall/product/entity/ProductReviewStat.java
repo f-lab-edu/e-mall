f5119ee981fffb3d9a7bfc23c08c3afc9232f5c0
kim-se-hee <saree98@nvaer.com>
2025-07-10 15:38:33 +0900

    fix: 올바른 soft delete 적용을 위해 애노테이션 위치 조정

--- src/main/java/ksh/emall/product/entity/ProductReviewStat.java
+++ src/main/java/ksh/emall/product/entity/ProductReviewStat.java
@@ -17,6 +17,8 @@ import org.hibernate.annotations.Where;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@SQLDelete(sql = "update order set is_deleted = true where id = ?")
+@Where(clause = "isDeleted = 0")
 public class ProductReviewStat extends BaseEntity {
 
     @Id
@@ -29,7 +31,5 @@ public class ProductReviewStat extends BaseEntity {
 
     private Long productId;
 
-    @SQLDelete(sql = "update order set is_deleted = true where id = ?")
-    @Where(clause = "isDeleted = 0")
     private boolean isDeleted;
 }
