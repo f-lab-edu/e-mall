0c15c8461cc38a4e5195b4f1168a8f71d3547627
kim-se-hee <saree98@nvaer.com>
2025-07-12 23:00:38 +0900

    feat: 제품의 리뷰 조회 구현

--- src/main/java/ksh/emall/review/service/ReviewService.java
+++ src/main/java/ksh/emall/review/service/ReviewService.java
@@ -4,9 +4,13 @@ import ksh.emall.product.entity.ProductReviewStat;
 import ksh.emall.product.repository.ProductRepository;
 import ksh.emall.product.repository.ProductReviewStatRepository;
 import ksh.emall.review.dto.request.ReviewRegisterRequestDto;
+import ksh.emall.review.dto.request.ReviewRequestDto;
 import ksh.emall.review.entity.Review;
 import ksh.emall.review.repository.ReviewRepository;
+import ksh.emall.review.repository.production.ReviewWithMember;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +22,18 @@ public class ReviewService {
     private final ProductRepository productRepository;
     private final ProductReviewStatRepository productReviewStatRepository;
 
+    @Transactional(readOnly = true)
+    public Page<ReviewWithMember> findReviewsOfProduct(
+        Pageable pageable,
+        Long productId,
+        ReviewRequestDto request
+    ) {
+        return reviewRepository.findByProductIdAfterCursor(
+            pageable,
+            productId,
+            request
+        );
+    }
 
     @Transactional
     public void register(long productId, ReviewRegisterRequestDto request) {
