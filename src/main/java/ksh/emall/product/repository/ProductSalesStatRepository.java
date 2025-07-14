1faa0cd327ffb957d7655146e7dc404cf7ac67f9
kim-se-hee <saree98@nvaer.com>
2025-07-14 16:26:00 +0900

    fix: 누락된 ProductSalesStatRepository 추가

---
+++ src/main/java/ksh/emall/product/repository/ProductSalesStatRepository.java
@@ -0,0 +1,7 @@
+package ksh.emall.product.repository;
+
+import ksh.emall.product.entity.ProductSalesStat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductSalesStatRepository extends JpaRepository<ProductSalesStat, Long> {
+}
