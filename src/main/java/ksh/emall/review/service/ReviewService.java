4d6c783ba7274b7bd07554ec697978771bf484c1
kim-se-hee <saree98@naver.com>
2025-07-22 20:10:54 +0900

    feat: 중복 리뷰 작성 방지 추가

--- src/main/java/ksh/emall/review/service/ReviewService.java
+++ src/main/java/ksh/emall/review/service/ReviewService.java
@@ -1,5 +1,7 @@
 package ksh.emall.review.service;
 
+import ksh.emall.common.exception.CustomException;
+import ksh.emall.common.exception.ErrorCode;
 import ksh.emall.product.entity.ProductReviewStat;
 import ksh.emall.product.repository.ProductRepository;
 import ksh.emall.product.repository.ProductReviewStatRepository;
@@ -16,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Clock;
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -42,10 +45,22 @@ public class ReviewService {
     @Transactional
     public Review register(long productId, ReviewRegisterRequestDto request) {
         productRepository.getById(productId);
+
+        boolean duplicated = reviewRepository.existsByProductIdAndMemberId(
+            productId,
+            request.getMemberId()
+        );
+        if (duplicated) {
+            throw new CustomException(ErrorCode.REVIEW_ALREADY_REGISTERED);
+        }
+
+
         Review review = createReview(productId, request);
         reviewRepository.save(review);
+
         ProductReviewStat productReviewStat = productReviewStatRepository.getByProductId(productId);
         productReviewStat.addReviewScore(request.getScore());
+
         return review;
     }
 
